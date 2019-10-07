ffcc0f32c29b3c002b0d252f702f41f109866425
Lucas Robitaille <lrjames237@gmail.com>
2019-10-07 18:48:24 -0500

    updated server

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,5 +3,9 @@ require('dotenv').config();
 module.exports = {
   "migrationsDirectory": "migrations",
   "driver": "pg",
-  "connectionString": process.env.DB_URL,
+  "connectionString": (process.env.DATABASE_URL === 'test')
+    ? process.env.TEST_DATABASE_URL
+    : process.env.DATABASE_URL,
+  "username": "user",
+  "password": "pass",
 }
